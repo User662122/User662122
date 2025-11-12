d7679ba1250692b4e4761de90092fbe32f682dd1
User662122 <gwkrishna06@gmail.com>
2025-11-12 10:57:54 +0530

    Update build.gradle.kts

--- chess_detector/app/build.gradle.kts
+++ chess_detector/app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -23,9 +22,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-        }
-        debug {
-            isDebuggable = true
         }
     }
 
@@ -41,14 +37,9 @@ android {
     buildFeatures {
         viewBinding = true
     }
-}
 
-repositories {
-    google()
-    mavenCentral()
-    // 👇 Local .aar folder ke liye
-    flatDir {
-        dirs("libs")
+    aaptOptions {
+        noCompress("tflite")
     }
 }
 
@@ -57,18 +48,19 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-
-    // ✅ Local OpenCV AAR (app/libs/opencv-4.10.0.aar)
+    
+    // OpenCV
     implementation(files("libs/opencv-4.10.0.aar"))
-
+    
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     
-    // ✅ NEW: OkHttp for backend communication
+    // OkHttp
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    
+    // TensorFlow Lite
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.14.0")
 }
